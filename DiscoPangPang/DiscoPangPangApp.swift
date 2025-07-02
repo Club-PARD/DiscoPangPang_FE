f9f0a8c6fe7ef4d06cf34d611bdbd37c2a8551f5
Lucid <bluecho97@gmail.com>
2025-07-02 10:48:48 +0900

    Initial Commit

---
+++ DiscoPangPang/DiscoPangPangApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiscoPangPangApp.swift
+//  DiscoPangPang
+//
+//  Created by  Lucid on 7/2/25.
+//
+
+import SwiftUI
+
+@main
+struct DiscoPangPangApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DiscoPangPangTests/DiscoPangPangTests.swift
@@ -0,0 +1,17 @@
+//
+//  DiscoPangPangTests.swift
+//  DiscoPangPangTests
+//
+//  Created by  Lucid on 7/2/25.
+//
+
+import Testing
+@testable import DiscoPangPang
+
+struct DiscoPangPangTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
